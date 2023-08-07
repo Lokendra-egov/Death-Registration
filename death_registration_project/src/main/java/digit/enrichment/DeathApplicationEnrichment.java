b2511994251c5bbdcaec12cca7f8c8708413fcc9
Lokendra Tyagi <lokendra.tyagi@egovernments.org>
2023-08-07 17:05:54 +0530

    first-commit

---
+++ death_registration_project/src/main/java/digit/enrichment/DeathApplicationEnrichment.java
@@ -0,0 +1,50 @@
+package digit.enrichment;
+
+import
+        digit.utils.IdgenUtil;
+import digit.web.models.AuditDetails;
+import digit.web.models.DeathRegistrationApplication;
+import digit.web.models.DeathRegistrationRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+@Component
+@Slf4j
+public class DeathApplicationEnrichment {
+
+    @Autowired
+    private IdgenUtil idgenUtil;
+
+
+    public void enrichDeathApplication(DeathRegistrationRequest deathRegistrationRequest) {
+        List<String> deathRegistrationIdList = idgenUtil.getIdList(deathRegistrationRequest.getRequestInfo(), deathRegistrationRequest.getDeathRegistrationApplications().get(0).getTenantId(), "dtr.registrationid", "", deathRegistrationRequest.getDeathRegistrationApplications().size());
+        Integer index = 0;
+        for(DeathRegistrationApplication application : deathRegistrationRequest.getDeathRegistrationApplications()){
+            // Enrich audit details
+            AuditDetails auditDetails = AuditDetails.builder().createdBy(deathRegistrationRequest.getRequestInfo().getUserInfo().getUuid()).createdTime(System.currentTimeMillis()).lastModifiedBy(deathRegistrationRequest.getRequestInfo().getUserInfo().getUuid()).lastModifiedTime(System.currentTimeMillis()).build();
+            application.setAuditDetails(auditDetails);
+
+            // Enrich UUID
+            application.setId(UUID.randomUUID().toString());
+
+            // Enrich registration Id
+            application.getAddressOfDeceased().setRegistrationId(application.getId());
+
+            // Enrich address UUID
+            application.getAddressOfDeceased().setId(UUID.randomUUID().toString());
+
+            //Enrich application number from IDgen
+            application.setApplicationNumber(deathRegistrationIdList.get(index++));
+
+        }
+    }
+
+    public void enrichDeathApplicationUponUpdate(DeathRegistrationRequest DeathRegistrationRequest) {
+        // Enrich lastModifiedTime and lastModifiedBy in case of update
+        DeathRegistrationRequest.getDeathRegistrationApplications().get(0).getAuditDetails().setLastModifiedTime(System.currentTimeMillis());
+        DeathRegistrationRequest.getDeathRegistrationApplications().get(0).getAuditDetails().setLastModifiedBy(DeathRegistrationRequest.getRequestInfo().getUserInfo().getUuid());
+    }
+}
---
+++ egov-workflow-v2/target/classes/db/migration/main/V20190322143035__wf_add_seq_ddl.sql
@@ -0,0 +1,2 @@
+CREATE SEQUENCE IF NOT EXISTS seq_eg_wf_state_v2 START 1;
+ALTER TABLE eg_wf_state_v2 ADD COLUMN seq INT;
